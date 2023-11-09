e01e7d9d2577949580bb6a23afb68eaf5c3423c7
Bhanu_Dova <130201202+bhanudova@users.noreply.github.com>
2023-11-09 09:08:39 +0530

    new changes added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,8 +9,8 @@ const Hero: React.FC<HeroProps> = ({ heading, message }) => {
     return (<>
         <div className="flex justify-center items-center h-screen bg-fixed bg-center bg-cover custom-img">
             {/* Overlay */}
-            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
-            <div className="p-5 text-white z-[2] mt-[-10rem]">
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2] h-screen" />
+            <div className="p-5 text-white z-[2]">
                 <h2 className="text-5xl font-bold">{heading}</h2>
                 <p className="py-5 text-xl">{message}</p>
                 <button className="px-8 py-2 border">Book</button>
